b15ced72189df781e9cacfad458638175b45c76b
kamalcody <kamal.s0408@gmail.com>
2026-06-11 22:18:17 +0530

    updated providerfileV1

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "azurerm" {
      resource_group_name = "rg-test-1" 
-      tenant_id            = "1e1fb270-a05a-405e-ae21-f0a6a237989c"
       storage_account_name = "terraformkamalstorage"
       container_name = "codecontainer"
       key = "pracctice.tfstate"
